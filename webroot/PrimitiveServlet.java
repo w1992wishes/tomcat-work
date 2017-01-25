ee7928df152de5f0400d825f65d0ac2b54029c0d
w1992wishes <WanQinFeng1992@163.com>
2017-01-25 20:28:09 +0800

    add
    Signed-off-by: w1992wishes <WanQinFeng1992@163.com>

---
+++ webroot/PrimitiveServlet.java
@@ -0,0 +1,29 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class PrimitiveServlet implements Servlet{
+
+    private Logger LOGGER = LoggerFactory.getLogger(PrimitiveServlet.class);
+    public void init(ServletConfig config) throws ServletException{
+        LOGGER.info("init");
+    }
+    public void service(ServletRequest request, ServletResponse response) throws IOException {
+        LOGGER.info("from service");
+        PrintWriter out = response.getWriter();
+        out.println("hello, roses are red");
+        out.print("violets are blue");
+    }
+    public void destroy(){
+        LOGGER.info("destroy");
+    }
+    public String getServletInfo(){
+        return null;
+    }
+    public ServletConfig getServletConfig(){
+        return null;
+    }
+}
